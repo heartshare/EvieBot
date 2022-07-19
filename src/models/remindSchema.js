b8b63c61040d01de370a216afce57e8ac2104084
Anne <92945218+vian-ai@users.noreply.github.com>
2022-07-19 16:05:20 -0600

    modified schema

--- src/models/remindSchema.js
+++ src/models/remindSchema.js
@@ -1,10 +1,11 @@
-import { Schema } from 'mongoose';
+import { Schema, model } from 'mongoose';
 
 const remindSchema = new Schema({
     userId: { type: String },
     reminder: { type: String },
     dateSet: { type: Date },
+    timer: { type: Date },
     remindId: { type: String }
 });
 
-export default remindSchema;
+export default model('Reminders', remindSchema);
